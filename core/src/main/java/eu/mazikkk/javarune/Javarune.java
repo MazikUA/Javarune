9475aa25e267b3eed8a7e55280fda345c493e059
MeiNanziiii <201730068+MeiNanziiii@users.noreply.github.com>
2025-06-27 18:37:30 +0300

    some random test code

--- core/src/main/java/eu/mazikkk/javarune/Javarune.java
+++ core/src/main/java/eu/mazikkk/javarune/Javarune.java
@@ -1,6 +1,8 @@
 package eu.mazikkk.javarune;
 
 import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -12,14 +14,27 @@ import com.badlogic.gdx.utils.viewport.FitViewport;
  * {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms.
  */
 public class Javarune implements ApplicationListener {
-    private Texture kris;
+    private Texture krisDown;
+    private Texture krisLeft;
+    private Texture krisRight;
+    private Texture krisUp;
     private SpriteBatch spriteBatch;
     private FitViewport viewport;
     private ShapeRenderer shapeRenderer;
 
+    private Texture kris;
+    private int x = 320 / 2 - 38 / 2;
+    private int y = 240 / 2 - 19 / 2;
+
     @Override
     public void create() {
-        kris = new Texture("kris.png");
+        krisDown = new Texture("kris_down.png");
+        krisLeft = new Texture("kris_left.png");
+        krisRight = new Texture("kris_right.png");
+        krisUp = new Texture("kris_up.png");
+
+        kris = krisDown;
+
         spriteBatch = new SpriteBatch();
         viewport = new FitViewport(320, 240);
         shapeRenderer = new ShapeRenderer();
@@ -32,6 +47,23 @@ public class Javarune implements ApplicationListener {
 
     @Override
     public void render() {
+        if (Gdx.input.isKeyPressed(Input.Keys.W)) {
+            kris = krisUp;
+            y += (int) (1);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.S)) {
+            kris = krisDown;
+            y -= (int) (1);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.A)) {
+            kris = krisLeft;
+            x -= (int) (1);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.D)) {
+            kris = krisRight;
+            x += (int) (1);
+        }
+
         ScreenUtils.clear(Color.BLACK);
 
         viewport.apply();
@@ -45,7 +77,7 @@ public class Javarune implements ApplicationListener {
         spriteBatch.setProjectionMatrix(viewport.getCamera().combined);
         spriteBatch.begin();
 
-        spriteBatch.draw(kris, 0, 0);
+        spriteBatch.draw(kris, x, y);
 
         spriteBatch.end();
     }
@@ -62,6 +94,10 @@ public class Javarune implements ApplicationListener {
 
     @Override
     public void dispose() {
+        krisDown.dispose();
+        krisLeft.dispose();
+        krisRight.dispose();
+        krisUp.dispose();
         kris.dispose();
         spriteBatch.dispose();
         shapeRenderer.dispose();
